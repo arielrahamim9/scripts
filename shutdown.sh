c38b2948007de0dcf10b6ec261619959d5b1cd32
arielrahamim9 <arielrahamim@protonmail.com>
2025-04-20 15:14:33 +0300

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -9,7 +9,7 @@ if [ "$uptime_minutes" -ge 180 ]; then
     INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
     # Stop the instance
     aws ec2 stop-instances --instance-ids "$INSTANCE_ID" --region "$AWS_DEFAULT_REGION"
-elif [ "$uptime_minutes" -ge 120 ]; then
+elif [ "$uptime_minutes" -ge 60 ]; then
     # Calculate minutes remaining until shutdown
     minutes_remaining=$((180 - uptime_minutes))
     echo "NOTICE: Instance has been running for $uptime_minutes minutes. Will stop in $minutes_remaining minutes."
